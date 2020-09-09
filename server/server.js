1059ec332b825b75194944ea6da474bef2f6d35c
Amanda Winkles <amandawinkles@gmail.com>
2020-09-09 17:01:05 -0500

    added diagram for proxy rendering all modules

--- server/server.js
+++ server/server.js
@@ -13,27 +13,27 @@ app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, './../public/index.html'));
 });
 
-//only want to proxy get request
-app.use('/proxy', proxy('www.google.com', {
-  filter: function(req, res) {
-    return req.method == 'GET';
-  }
-}));
+// //only want to proxy get request
+// app.use('/proxy', proxy('www.google.com', {
+//   filter: function(req, res) {
+//     return req.method == 'GET';
+//   }
+// }));
 
-app.use('/images', proxy('http://localhost:3001', {
-  proxyReqPathResolver: function (req) {
-    //localhost:3000/?10
-    //console.log('req.url: ', req.url);
-    let pieces = req.url.split('?');
-    let pathname = pieces[0];
-    let queryString = pieces[1];
-    if (!queryString) {
-      return '';
-    } else {
-      return `${pathname}?${queryString}`;
-    }
-  }
-}));
+// app.use('/images', proxy('http://localhost:3001', {
+//   proxyReqPathResolver: function (req) {
+//     //localhost:3000/?10
+//     //console.log('req.url: ', req.url);
+//     let pieces = req.url.split('?');
+//     let pathname = pieces[0];
+//     let queryString = pieces[1];
+//     if (!queryString) {
+//       return '';
+//     } else {
+//       return `${pathname}?${queryString}`;
+//     }
+//   }
+// }));
 
 
 app.listen(PORT, () => {
