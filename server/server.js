23c3471cf55bb8dcf44da00cdcc77ec500cf5cf8
Amanda Winkles <amandawinkles@gmail.com>
2020-09-11 17:31:23 -0500

    all four modules running on proxy

--- server/server.js
+++ server/server.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const cors = require('cors');
-const proxy = require('express-http-proxy');
 const path = require('path');
 const PORT = 3000;
 const app = express();
@@ -13,27 +12,6 @@ app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, './../public/index.html'));
 });
 
-// //only want to proxy get request
-// app.use('/proxy', proxy('www.google.com', {
-//   filter: function(req, res) {
-//     return req.method == 'GET';
-//   }
-// }));
-
-// app.use('/images', proxy('http://localhost:3001', {
-//   proxyReqPathResolver: function (req) {
-//     //localhost:3000/?10
-//     //console.log('req.url: ', req.url);
-//     let pieces = req.url.split('?');
-//     let pathname = pieces[0];
-//     let queryString = pieces[1];
-//     if (!queryString) {
-//       return '';
-//     } else {
-//       return `${pathname}?${queryString}`;
-//     }
-//   }
-// }));
 
 
 app.listen(PORT, () => {
